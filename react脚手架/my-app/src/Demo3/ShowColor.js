aa6ddff1d7edf83a201b5ed908890fb0e4f9f3dc
wenlibing <libing.wen@hand-china.com>
2021-11-24 11:40:17 +0800

    react hooks todoList练习

--- react脚手架/my-app/src/Demo3/ShowColor.js
+++
@@ -1,9 +0,0 @@
-import React, { useContext } from 'react'
-import { ColorContext } from './color'
-
-export default function ShowColor() {
-    let { color } = useContext(ColorContext)
-    return (<h2 style={{ color: color }}>
-        当前的颜色为{color}
-    </h2>)
-}
